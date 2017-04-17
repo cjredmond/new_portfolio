8765f1e00e6511749a25cfeadbb558e5138b9b32
cjredmond <connor.redmond@gmail.com>
2017-04-17 15:14:38 -0400

    added resume

--- new_portfolio/urls.py
+++ new_portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from portfolio.views import IndexView
+from portfolio.views import IndexView, ResumeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name="index_view"),
+    url(r'^resume/$', ResumeView.as_view(), name='resume_view')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class ResumeView(TemplateView):
+    template_name = 'resume.html'
