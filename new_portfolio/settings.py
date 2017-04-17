6149343d4d5284cd13da12d38eee889032d72f6d
cjredmond <connor.redmond@gmail.com>
2017-04-17 15:29:55 -0400

    added database default in settings

--- new_portfolio/settings.py
+++ new_portfolio/settings.py
@@ -70,6 +70,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+if db_from_env:
+    DATABASES['default'] = db_from_env
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
